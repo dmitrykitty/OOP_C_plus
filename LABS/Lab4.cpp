f66b594ce0981cf08e3dac884f6ea9d08c60bde4
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2025-03-26 18:47:03 +0100

    Added changes to LAB4

--- LABS/Lab4.cpp
+++ LABS/Lab4.cpp
@@ -5,9 +5,11 @@ class S {
   size_t capacity{};
   size_t size{};
   char *data{};
+  inline static int capacit=0;
 
 public:
-  S(const char* s): capacity(strlen(s) + 1), size(capacity - 1), data(new char[capacity]{}) {}
+  S(const char* s): capacity(strlen(s) + 1), size(capacity - 1), data(new char[capacity]{}) {
+  }
   S(const S& other): S(other.data) {} // konstruktor kopiujący
   S(S&& other): S(other.data) {} //konstruktor przenoszący
 
