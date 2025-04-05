07262749af575bee2bcff4cf8ab8753a9141db24
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2025-04-06 00:26:52 +0200

    filterwater class added

---
+++ ex4-2-inheritance/filter_water.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+enum type_filter_water {
+    flt_none = 0,
+    flt_mechanical = 1,
+    flt_aragon = 2,
+    flt_calcium = 3
+};
+
+class FilterWater {
+protected:
+    unsigned date{0};
+    type_filter_water type{flt_none};
+
+public:
+    FilterWater(type_filter_water tp = flt_none, unsigned date = 0): type(tp), date(date) {}
+    unsigned get_date() const { return date; }
+    type_filter_water get_type() const { return type; }
+};
+
+class Mechanical : public FilterWater {
+public:
+    Mechanical(unsigned date): FilterWater(flt_mechanical, date) {}
+};
+
+class Aragon : public FilterWater {
+public:
+    Aragon(unsigned date): FilterWater(flt_aragon, date) {}
+};
+
+class Calcium : public FilterWater {
+public:
+    Calcium(unsigned date): FilterWater(flt_calcium, date) {}
+};
+
+class GeyserClassic {
+public:
+    enum { total_slots = 3 };
+
+private:
+    FilterWater *slots[total_slots]{nullptr};
+
+public:
+    GeyserClassic(FilterWater *f1 = nullptr, FilterWater *f2 = nullptr, FilterWater *f3 = nullptr) {
+        add_filter(1, f1);
+        add_filter(2, f2);
+        add_filter(3, f3);
+    }
+
+    const FilterWater *operator[](int index) const;
+
+    void add_filter(int slot_num, FilterWater *filter);
+};
+
+
+const FilterWater *GeyserClassic::operator[](int index) const {
+    if (index < 0 || index >= total_slots || slots[index] == nullptr)
+        return nullptr;
+    return slots[index];
+}
+
+void GeyserClassic::add_filter(int slot_num, FilterWater *filter) {
+    if (!slots[slot_num - 1] && static_cast<type_filter_water>(slot_num) == filter->get_type())
+        slots[slot_num - 1] = filter;
+}
+
+int main() {
+    return 0;
+}
+
