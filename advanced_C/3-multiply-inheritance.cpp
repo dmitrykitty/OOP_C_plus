f17d008621176acef59c73d5ae9629d844564a9b
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2025-04-25 03:12:55 +0200

    more theory added

--- advanced_C/3-multiply-inheritance.cpp
+++ advanced_C/3-multiply-inheritance.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <ostream>
 
-struct Dad {
+struct Granny {
+    int g;
+};
+
+struct Dad: virtual public Granny{
     int x;
 };
 
-struct Mom {
+struct Mom: virtual public Granny {
     int y;
 };
 
-struct Son : public Dad, public Mom {
+struct Son :  public Dad, public Mom {
     int z;
+
 };
 
 /*w pamieci:
@@ -23,9 +28,18 @@ jak robimy cast z Syna do Mom, wskaznik sie przesunie o 4 baity;
 int main() {
     Son s;
     Son* ptr_son = &s;
+    Mom* ptr_mom = new Mom;
+    Dad* ptr_dad = new Dad;
+    Granny* ptr_granny = new Granny;
+    std::cout << ptr_granny << " " << ptr_son <<  std::endl;
+    static_cast<Son*>(ptr_dad); //działa
+    static_cast<Son*>(ptr_mom); //działa
+    static_cast<Son*>(ptr_granny); //nie dzaiła, czyli w dół nie zbyt działa
+
 
     Mom* ptr_mom = &s; //czyli tutaj przesuniecie o 4 baity do przodu, bo obiekt Mom zaczyna sie poźniej
     std::cout << ptr_son << " " << ptr_mom << std::endl;
+    std::cout << sizeof(s) << std::endl;
 
     return 0;
 }
