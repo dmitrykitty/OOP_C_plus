8806999cfbe66da0fbbb726a15f2775f5e9622bb
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2025-05-02 23:35:48 +0200

    practise 2 started

---
+++ ex5-4-exception-object/practise-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+class ConvertTolnt {
+
+};
+
+int main()
+{
+    std::string digit;
+    getline(std::cin, digit);
+
+    const char* str = digit.c_str(); // массив символов char с прочитанной строкой
+
+    // здесь продолжайте функцию main
+
+
+    return 0;
+}
