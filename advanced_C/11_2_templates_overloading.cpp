88db6c9919969fcd63b1c148056e4f8239b8f52a
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2025-06-09 15:54:16 +0200

    template advance theory added

--- advanced_C/11_2_templates_overloading.cpp
+++ advanced_C/11_2_templates_overloading.cpp
@@ -1,13 +1,99 @@
-template <typename T>
-void f(T x){}
+#include <iostream>
 
-void f(int x){}
+template<typename T>
+void f(T x) {
+    std::cout << 1;
+}
+
+void f(int x) {
+    std::cout << 2;
+}
+
 //im dokładniej jest opisana sygnatura funkcji, będzie ona wywołana
 //pisząc wybor typu w <> - będzie wywołana template wersja
 //szablony o jednakowych sygnaturach, ale roznych zwracanych typach się kompilują,
 //ale nie da się je wywołać
 
+template<typename T>
+void g(const T& x) {
+    std::cout << 1;
+}
+
+template<typename T>
+void g(T& x) {
+    std::cout << 2;
+}
+
+//-----------------------------------specialization----------------------------------------
+template<typename T>
+class vector {
+    T* arr;
+};
+
+//pełna specjalizacja
+template<>
+class vector<bool> {
+    char* arr;
+};
+
+//-----------------------------------------
+template<typename T, typename U>
+struct S {};
+
+//częściowa specjalizacja
+template<typename T>
+struct S<T, T> {};
+
+template<typename T>
+struct S<int, T> {};
+
+//------------------------------------------
+
+template<typename W>
+struct A {
+    A() = delete; //zeby sprawdzic jaki typ bedzie wywołany
+};
+
+//dla wszystkich obiektow tworzonych przez referencje inny specjalizacja(czesciowa specjalizacja)
+template<typename W>
+struct A<W &> {
+    A() = delete;
+};
+
+template<typename W>
+struct A<const W> {
+    A() = delete;
+};
+
+
+// --------------------------function TEMPLATE specialization--------------------------------
+template<typename T, typename U>
+void m(T, U) { std::cout << 1; }
+
+//overloading - najpierw wybierana funkcja przeciazona, a pozniej specjalizacja
+//czyli w tym przypadku wybierany drugi template i u niego jest specjalizacja, wiec bedzie wybrany 3.
+//w innym przypadku najbardziej pasujaca przeciazona funkcja - 2 i będzie ona wybrana
+template<typename T>
+void m(T, T) { std::cout << 2; }
+
+//u funkcji jest tylko pełna specjalizacja
+template<>
+void m(int, int) { std::cout << 3; }
+
+
+
 int main() {
     int x = 0;
     f(x); //będzie wywołana 2 funkcja
+
+    g(x); //2, ale jak bedzie w drugiej T x - juz CE -> miedzy const T& i T CE, miedzy const T i T - CE
+    const int& y = x;
+    g(y); //1
+    S<double, double> s; //CE, bo nie ma lepszej
+
+    //A<const int&> a; //A<W&> - referencja ma priorytet
+
+    long r = 0;
+    m(0, r); //1
+    m(0, 0); //bedzie wypisane to, co jest na samym koncu 2/3
 }
---
+++ advanced_C/11_3_non_type_template_params.cpp
@@ -0,0 +1,30 @@
+#include <compare>
+#include <iostream>
+#include <vector>
+//moze byc inregral ypes + od C++ 20 float, double
+//ale nie mozna przekazywac wtedy zmienne, dopoki ta zmiena nie jest constexpr
+
+template<typename T, size_t N>
+class array {
+    T arr[N];
+};
+
+template <size_t M, size_t N, typename T = double>
+class Matrix {
+    std::vector<std::vector<T>> data;
+    Matrix(): data(M, std::vector<T>(N)){}
+};
+
+template<size_t N, typename T = double>
+using SquareMatrix = Matrix<N, N, T>;
+
+template<size_t M, size_t K, size_t N, typename T>
+Matrix<M, N, T> operator*(const Matrix<M, K, T>& a, const Matrix<K, N, T>& b);
+
+//Template template params
+template<typename T, typename K, template<typename, typename> class Container = std::vector<T>>
+class Stack {
+    Container<T, K> cont; //czyli Stack jest obudową nad template class, gdzie jako default jest Vector
+};
+
+int main() {}
